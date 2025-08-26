de8af95160cb22d1754f1ec8488d3b5cf36e61f6
0thigs <machinimavlogs@gmail.com>
2025-08-25 23:42:18 -0300

    feat: integrate Gluestack UI components and update Tailwind configuration for improved theming

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { Text, View } from 'react-native';
 
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+
 import './global.css';
 
 export default function App() {
   return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-2xl font-bold text-blue-500">Hello World</Text>
-    </View>
+    <GluestackUIProvider mode="light"><View className="flex-1 justify-center items-center bg-white">
+        <Text className="text-2xl font-bold text-blue-500">Hello World</Text>
+      </View></GluestackUIProvider>
   );
 }

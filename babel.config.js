d2d9a175f7f54e75b7a220118ca556bcd8bfd1a3
0thigs <machinimavlogs@gmail.com>
2025-09-01 07:38:00 -0300

    Revert "Merge branch 'main' of https://github.com/0thigs/recipe-flow"
    This reverts commit e831795b637d49f8842ed99ca30f6db2a6233f6b, reversing
    changes made to 091722e72f12b56c1f330f07c71aede1887ce2a2.

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+    presets: ['babel-preset-expo'],
+    plugins: [
+      'react-native-css-interop/babel', // necessário no NativeWind v4
+      'nativewind/babel',
+      // aliases opcionais
+      ['module-resolver', { alias: { '~': './src', '@': './' } }],
+      'react-native-reanimated/plugin', // SEMPRE por último
     ],
-    plugins: ['react-native-reanimated/plugin']
   };
 };
